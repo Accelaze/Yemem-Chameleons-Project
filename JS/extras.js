c6c96021b8a5b91d0a0c14f52bb58a77344f6a45
T1gr0 <beaumontalanlins@gmail.com>
2024-10-20 22:12:07 +0100

    Working on it

--- JS/extras.js
+++ JS/extras.js
@@ -1,4 +1,29 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+function updateDarkmode() {
+    let darkmode = localStorage.getItem("darkmode") === "true";
+    let root = document.querySelector(":root"); 
+    if (darkmode) {
+        root.style.setProperty("--text", "#000000");
+        root.style.setProperty("--background", "#FFFFFF");
+    }
+    else {
+        root.style.setProperty("--text", "#FFFFFF");
+        root.style.setProperty("--background", "#000000");
+    }
+}
+
+function toggleDarkmode(checkbox) {
+    if (checkbox.checked) {
+        localStorage.setItem("darkmode", "true");
+    }
+    else {
+        localStorage.setItem("darkmode", "false");
+    }
+    updateDarkmode();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.getElementById("darkmode");
+    checkbox.checked = localStorage.getItem("darkmode") === "true";
+
+    updateDarkmode();
+});
